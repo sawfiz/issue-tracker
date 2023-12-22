d7d4e9ab3672c4dc5b5f3334ae5f065ec0260157
sawfiz <112825751+sawfiz@users.noreply.github.com>
2023-12-22 20:02:07 +0800

    Set up Radix UI

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+
+const NewIssuePage = () => {
+  return (
+    <Button>Create New Issue</Button>
+  )
+}
+
+export default NewIssuePage 
---
+++ app/issues/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+
+const IssuesPage = () => {
+  return (
+    <Button>New Issue</Button>
+  )
+}
+
+export default IssuesPage 
