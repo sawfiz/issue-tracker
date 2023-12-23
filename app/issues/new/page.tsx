a1e91ff02a056f1f74ff8fd857326cf5c37cd671
sawfiz <112825751+sawfiz@users.noreply.github.com>
2023-12-23 22:26:51 +0800

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,62 @@
 import { TextField, Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, useController, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+// Use an interface to define the shape of form data
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  // <> is a `generic` in TypeScript
+  // Passing `IssueForm` as a generic parameter to `useForm()` to specify
+  // the shape of the form data that will be managed by `useForm`.
+  const {
+    // This method allows you to register an input or select element and apply validation rules to React Hook Form.
+    register,
+    // This function will receive the form data if form validation is successful.
+    handleSubmit,
+    // To be used in <Controller>
+    control,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm<IssueForm>();
+
+  const router = useRouter();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+      className="max-w-xl space-y-3"
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        {/* Use spread operator as `register` returns an object of 4 fields */}
+        {/* name, onChange, onBlue, ref */}
+        <TextField.Input {...register("title")} placeholder="Title" />
       </TextField.Root>
-      <SimpleMDE />
+      {/* SimpleMED does not support additional props, so cant use `register` */}
+      {/* Use the Controller component instead */}
+
+      {/* SimpleMDE does not accept additonal props
+      Use Controller to register it with react-hook-form */}
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
