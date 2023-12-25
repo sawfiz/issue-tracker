955eb6a6a0a9472894de41f94517d1f10b73c4ae
sawfiz <112825751+sawfiz@users.noreply.github.com>
2023-12-25 17:19:22 +0800

    Build styled Link component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import prisma from "@/prisma/client";
-import { notFound } from "next/navigation";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import delay from "delay";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 // For some reason writing this inline cause compile error
 interface Props {
@@ -15,11 +14,10 @@ const IssuesDetailPage = async ({ params }: Props) => {
   if (isNaN(parseInt(params.id))) notFound();
 
   // params is an object {id: "123"}
-  // use parseInt() to convert string to an Int
+  // use parseInt() to convert string to an InNaNt
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
-  await delay(1000);
 
   // The default not found page
   if (!issue) notFound();
@@ -32,7 +30,9 @@ const IssuesDetailPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.updatedAt.toDateString()}</Text>
       </Flex>
-      <Card>{issue.description}</Card>
+      <Card className="prose lg:prose-xl mt-4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 };
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,11 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import delay from "delay";
+import Link from "@/app/components/Link"
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueToolBar from "../components/IssueToolBar";
-import Link from "next/link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(1000);
 
   return (
     <div>
