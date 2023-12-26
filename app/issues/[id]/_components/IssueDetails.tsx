24e3d1b60b6d7f4abe28d2bb6f46afd6f67c258d
sawfiz <112825751+sawfiz@users.noreply.github.com>
2023-12-26 08:15:03 +0800

    Refactor: Apply the single responsibility principle in IssueDetailPage

---
+++ app/issues/[id]/_components/IssueDetails.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Heading, Flex, Text, Card } from "@radix-ui/themes";
+import { IssueStatusBadge } from "@/app/components";
+import ReactMarkdown from "react-markdown";
+import { Issue } from "@prisma/client";
+
+const IssueDetails = ({issue}:{issue: Issue}) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+
+      <Flex gap="4" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.updatedAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose lg:prose-xl mt-4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
---
+++ app/issues/[id]/_components/index.ts
@@ -0,0 +1,2 @@
+export {default as IssueDetails} from "./IssueDetails"
+export {default as EditIssueButton} from "./EditIssueButton"
