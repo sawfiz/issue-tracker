fe1401f0310e093d5b882a91e7dceaa2187811fb
sawfiz <112825751+sawfiz@users.noreply.github.com>
2023-12-28 16:35:21 +0800

    Add a toaster error

--- app/issues/[id]/_components/AssignSelect.tsx
+++ app/issues/[id]/_components/AssignSelect.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssignSelect = ({ issue }: { issue: Issue }) => {
   const router = useRouter();
@@ -29,33 +30,36 @@ const AssignSelect = ({ issue }: { issue: Issue }) => {
 
   const handleChange = async (newOwnerId: string) => {
     try {
-      const result = await axios.patch(`/api/issues/${issue.id}`, {
-        ownerId: newOwnerId === "UNASSIGNED" ? null: newOwnerId,
+      const result = await axios.patch(`/xapi/issues/${issue.id}`, {
+        ownerId: newOwnerId === "UNASSIGNED" ? null : newOwnerId,
       });
       router.push("/issues");
     } catch (error) {
-      console.log(error);
+      toast.error("Changes could not be saved.");
     }
   };
 
   return (
-    <Select.Root
-      defaultValue={issue.ownerId || ""}
-      onValueChange={(newOwnerId) => handleChange(newOwnerId)}
-    >
-      <Select.Trigger placeholder="Assign Owner"></Select.Trigger>
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Users</Select.Label>
-          <Select.Item value="UNASSIGNED" >Unassigend</Select.Item>
-          {users.map((user: User) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.ownerId || ""}
+        onValueChange={(newOwnerId) => handleChange(newOwnerId)}
+      >
+        <Select.Trigger placeholder="Assign Owner"></Select.Trigger>
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Users</Select.Label>
+            <Select.Item value="UNASSIGNED">Unassigend</Select.Item>
+            {users.map((user: User) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
