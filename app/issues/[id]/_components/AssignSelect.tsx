8e3ed7d52b64a6f5a17661a99d049a104a635470
sawfiz <112825751+sawfiz@users.noreply.github.com>
2023-12-28 12:14:17 +0800

    Fetch data with react query

--- app/issues/[id]/_components/AssignSelect.tsx
+++ app/issues/[id]/_components/AssignSelect.tsx
@@ -1,20 +1,40 @@
 "use client";
 import { Select } from "@radix-ui/themes";
-import { User } from "@prisma/client";
-import { useEffect, useState } from "react";
+// import { User } from "@prisma/client";
+// import { useEffect, useState } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { User } from "@prisma/client";
+import Skeleton from "@/app/components/Skeleton";
 
 const AssignSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  // const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const {data} = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const { data } = await axios.get<User[]>("/api/users");
+  //     setUsers(data);
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
+
+  const {
+    isPending,
+    error,
+    data: users,
+  } = useQuery<User[]>({
+    // queryKey is an identifier for data in the cache
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    retry: 3,
+    staleTime: 60 * 1000, // 60s
+  });
+
+  if (isPending) return <Skeleton height="2rem" />;
+
+  // If error, retun nothing. The Select component disappers.
+  if (error) return;
 
   return (
     <Select.Root>
@@ -22,8 +42,10 @@ const AssignSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Users</Select.Label>
-          {users.map((user) => (
-            <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+          {users.map((user: User) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
           ))}
         </Select.Group>
       </Select.Content>
