36f712be9919d07749de97d31a2264a7f2701aa4
sawfiz <112825751+sawfiz@users.noreply.github.com>
2024-01-03 12:46:12 +0800

    Finish LatestIssues

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -1,22 +1,59 @@
 import prisma from "@/prisma/client";
-import axios from "axios";
-import { Issue } from "@prisma/client";
+import {
+  Avatar,
+  Card,
+  Flex,
+  Heading,
+  Table,
+  TableColumnHeaderCell,
+  Text,
+} from "@radix-ui/themes";
+import { IssueStatusBadge } from "./components";
 
 const LatestIssues = async () => {
   // Fetch using Prisma
   const issues = await prisma.issue.findMany({
     orderBy: { createdAt: "desc" },
     take: 5,
+    include: {
+      owner: true,
+    },
   });
 
   return (
     <>
-      <div>Latest Issues</div>
-      <ul>
-        {issues.map((issue) => (
-          <li key={issue.id}>{issue.title}</li>
-        ))}
-      </ul>
+      <Card>
+        <Heading size="4" mb="5">
+          Latest Issues
+        </Heading>
+        <Table.Root>
+          <Table.Body>
+            {issues.map((issue) => {
+              return (
+                <Table.Row key={issue.id}>
+                  <Table.Cell>
+                    <Flex justify="between">
+                      <Flex direction="column" align="start">
+                        <Text>{issue.title}</Text>
+                        <IssueStatusBadge status={issue.status} />
+                      </Flex>
+                      {issue.owner && (
+                        <Avatar
+                          src={issue.owner.image!}
+                          fallback="?"
+                          size="2"
+                          radius="full"
+                        />
+                      )}
+                    </Flex>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+            <Table.Row></Table.Row>
+          </Table.Body>
+        </Table.Root>
+      </Card>
     </>
   );
 };
