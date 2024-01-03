af04ae912d29e78d1980d4ac865a5edd110e8ec7
sawfiz <112825751+sawfiz@users.noreply.github.com>
2024-01-03 14:51:24 +0800

    Make IssueChart clickable

--- app/IssueChart.tsx
+++ app/IssueChart.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { Status } from "@prisma/client";
 import { Card } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 interface Props {
@@ -8,12 +10,25 @@ interface Props {
   closed: number;
 }
 
+interface data {
+  label: string;
+  value: number;
+  status: Status;
+}
+
 const IssueChart = ({ open, inProgress, closed }: Props) => {
+  const router = useRouter();
+
   const data = [
-    { label: "Open", value: open },
-    { label: "In Progress", value: inProgress },
-    { label: "Closed", value: closed },
+    { label: "Open", value: open, status: "OPEN" },
+    { label: "In Progress", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed", value: closed, status: "CLOSED" },
   ];
+
+  const handleClick = (data: data) => {
+    router.push(`/issues?status=${data.status}`);
+  };
+
   return (
     <Card>
       <ResponsiveContainer width="100%" height={300}>
@@ -22,6 +37,8 @@ const IssueChart = ({ open, inProgress, closed }: Props) => {
             dataKey="value"
             barSize={60}
             style={{ fill: "var(--accent-9)" }}
+            onClick={handleClick}
+            className="cursor-pointer"
           />
           <XAxis dataKey="label" />
           <YAxis />
