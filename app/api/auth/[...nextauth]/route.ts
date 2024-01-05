ae5e4af564364a33bef5b99e8a0e0890d71f732f
sawfiz <112825751+sawfiz@users.noreply.github.com>
2024-01-05 08:48:09 +0800

    Bug fix: use correct @next-auth/prisma-adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-
+import NextAuth from "next-auth";
 import authOptions from "@/app/auth/authOptions";
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
